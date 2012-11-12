4a5d69a3f7f3ced3c4de01de982a2d573f9f199d
Travis Holton <travis@catalyst.net.nz>
2012-11-13 12:48:39 +1300

    make sure we're only getting active menus

--- lib/Vindaloo/Orders.pm
+++ lib/Vindaloo/Orders.pm
@@ -30,6 +30,7 @@ sub random_curry {
         {
             'category.name'   => $category,
             'spiceyness.name' => $spiceyness,
+            'me.active'       => 1
         },
         {
             join => [
@@ -39,7 +40,7 @@ sub random_curry {
         }
     );
     my @ids          = $eligible_curries->get_column('me.id')->all;
-    my $random_id    = $ids[ int( rand(scalar @ids) ) ];
+    my $random_id    = $ids[ int( rand( scalar @ids ) ) ];
     my $random_curry = $curry_menu->find($random_id);
     $self->create_curry_order( $random_curry, $spice_obj );
 
