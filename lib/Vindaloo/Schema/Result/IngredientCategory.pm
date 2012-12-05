8926b50854e761d8c0219028d62af519ff96b625
William Travis Holton <wtholton@googlemail.com>
2012-12-06 08:00:22 +1300

    tidied up; refactoring a query

--- lib/Vindaloo/Schema/Result/IngredientCategory.pm
+++ lib/Vindaloo/Schema/Result/IngredientCategory.pm
@@ -2,19 +2,16 @@ package Vindaloo::Schema::Result::IngredientCategory;
 
 use Moose;
 use namespace::autoclean;
-BEGIN {extends 'DBIx::Class::Core'};
+BEGIN { extends 'DBIx::Class::Core' }
 
 __PACKAGE__->table('ingredient_category');
 __PACKAGE__->add_columns(qw/id name/);
 __PACKAGE__->set_primary_key('id');
 
+__PACKAGE__->has_many(
+    base_ingredients => BaseIngredient => { 'foreign.category' => 'self.id' } );
 
-__PACKAGE__->has_many(base_ingredients => BaseIngredient =>
-{'foreign.category' => 'self.id'} );
-
-
-__PACKAGE__->meta->make_immutable(inline_constructor => 0);
-
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 
 1;
 
